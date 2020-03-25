319c0ad83a7228eeefdbbd51de1fd51a5626b07e
bloodbone <k0451212@gmail.com>
2020-03-25 11:03:39 +0800

    upload

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: ['./src/index.jsx'],
   output: {
-    filename: './src/bundle.js',
+    filename: './public/bundle.js',
     path: path.resolve(__dirname, './'),
   },
   module: {
